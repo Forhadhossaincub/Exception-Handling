99554f4daff3e1695f19defd23dc99ab6a57043f
NET <NET@DESKTOP-NSMUMFG>
2022-10-16 12:21:00 +0600

    Add project files.

---
+++ Exception Handling/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+                Console.WriteLine("Software Development Day 01");
+
+                Console.Write("Enter Your Name : ");
+                string name = Console.ReadLine();
+
+                Console.Write("Enter Your Age : ");
+                int age = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter Your MontlySalary : ");
+                decimal salary = decimal.Parse(Console.ReadLine());
+
+                Console.WriteLine($" Your Name is:{name} . Your Age is :{age} . Your Montly Salary is :{salary} ");
+
+                Console.ReadKey();
+
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+
+    }
+}
+
